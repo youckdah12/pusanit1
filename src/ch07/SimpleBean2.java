0715e90312c87cdeeecf0988074838d4dc9867cd
it <it@DESKTOP-T8TTLRQ>
2019-04-05 15:23:02 +0900

    20190404 pm3:22 서버프로그램구현 -수정 작업 할때 -오타난거 고침 - 좋은기능 굳잡!

---
+++ src/ch07/SimpleBean2.java
@@ -0,0 +1,32 @@
+package ch07;
+
+public class SimpleBean2 {
+
+	private String name;
+	private int age;
+	private String city;
+	
+	
+	//getXxx
+	public String getName() {
+		return name;
+	}
+	//setXxx(DateType xxx)
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+}
